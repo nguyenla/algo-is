7c139718f269d845f9935b4a7d2477cee4303d23
nguyenla <phulamnjc@gmail.com>
2016-11-12 19:35:58 -0500

    Add Working_restaurant.java. Working in progress

---
+++ Working_restaurant.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Working_restaurant {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		s.nextLine();
+		while (n!= 0) {
+			Queue<String> queue = new ArrayDeque<String>();
+			for (int i = 0; i < n; i++) {
+				queue.add(s.nextLine());
+			}
+			process(queue);
+			
+			n = s.nextInt();
+			s.nextLine();
+		}
+	}
+	
+	public static void process(Queue<String> input) {
+		int stack1 = 0;
+		int stack2 = 0;
+		
+		ArrayList<String> res = new ArrayList<String>();
+		while (!input.isEmpty()) {
+			while (!input.isEmpty() && input.peek().substring(0,4).equals(("DROP"))) {
+				String temp = input.remove();
+				int num = new Integer(temp.substring(5));
+				res.add("DROP 2 " + num);
+				stack2+= num;
+			}
+			if (input.isEmpty()) {
+				break;
+			}
+			String take;
+			while (!input.isEmpty() && input.peek().substring(0,4).equals(("TAKE"))) {
+				take = input.remove();
+				int num = new Integer(take.substring(5));
+				if (stack1 == 0) {
+					res.add("MOVE 2->1 " + stack1);
+					stack1 += stack2;
+					stack2 = 0;
+					res.add("TAKE 1 " + num);
+					stack1 -= num; // Take num from stack 1
+				}
+				else {
+					if (stack2 < num) {
+						
+					}
+				}
+			}
+		}
+		
+//		while (i < input.length && input[i].substring(0, 4).equals("DROP")) {
+//			int numPlates = new Integer(input[i].substring(5));
+//		}
+	}
+}
